30f499b6b6100af41a36c1764af6f7a29b69faf2
Joe0lsen <olsen.jcm@gmail.com>
2023-06-05 14:46:04 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Title Generator
+by Joe Olsen
+
+## Overview
+This purpose of this webpage is to **generate a title** using random words (of specific types).
+
+This generator takes a random adverb, adjective and noun to create a random title.
+
+These words are obtained from the **faker.js** *Words* module.
+
+I did this project because I had searched the web for a random title generator, but all I could find were name generators and sentence generators. I could never find what I was specifically looking for, so I scratched my own itch.
+
+[Demo Video](https://youtu.be/_i1SOkF7aNI)
+
+## Development Environment
+
+- VSCode
+- Chrome
+
+## Relevant Websites
+
+- [faker.js Words](https://fakerjs.dev/api/word.html)
+- [w3schools.com](https://www.w3schools.com/jsref/)
+
+## Future Work
+
+- Deploy webpage
+- Be able to remove specific titles from "Stored Titles"
---
+++ scripts/script.js
@@ -0,0 +1,105 @@
+import { faker } from "https://cdn.skypack.dev/@faker-js/faker";
+
+const w = faker.word;
+
+const storedTitles = getCookie("storedTitles").split(",");
+
+function Capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function generateTitle() {
+  let adv = Capitalize(w.adverb());
+  let adj = Capitalize(w.adjective());
+  let noun = Capitalize(w.noun());
+
+  renderTitle(adv, adj, noun);
+}
+
+function renderTitle(adv, adj, noun) {
+  let advCheck = document.querySelector("#advCheck");
+  let adjCheck = document.querySelector("#adjCheck");
+  let nounCheck = document.querySelector("#nounCheck");
+
+  let advBox = document.querySelector("#advText");
+  let adjBox = document.querySelector("#adjText");
+  let nounBox = document.querySelector("#nounText");
+
+  if (advCheck.checked == false) {
+    advBox.value = adv;
+  }
+  if (adjCheck.checked == false) {
+    adjBox.value = adj;
+  }
+  if (nounCheck.checked == false) {
+    nounBox.value = noun;
+  }
+
+  let title = document.querySelector("#generatedTitle");
+  title.innerHTML = `${advBox.value} ${adjBox.value} ${nounBox.value}`;
+}
+
+function renderStoredTitles() {
+  let storedTitlesBox = document.querySelector("#storedTitles");
+  if (getCookie("storedTitles") != "") {
+    storedTitlesBox.innerHTML = "";
+    storedTitles.forEach(function (title) {
+      let addedTitle = document.createElement("p");
+      addedTitle.innerText = title;
+      storedTitlesBox.appendChild(addedTitle);
+    });
+  } else {
+    storedTitlesBox.innerHTML = "<p>No Stored Titles Yet</p>";
+  }
+}
+
+function storeTitle() {
+  let advBox = document.querySelector("#advText");
+  let adjBox = document.querySelector("#adjText");
+  let nounBox = document.querySelector("#nounText");
+
+  let title = `${advBox.value} ${adjBox.value} ${nounBox.value}`;
+  storedTitles.push(title);
+  setCookie("storedTitles", storedTitles.join(), 30);
+
+  renderStoredTitles();
+}
+
+function clearStoredTitles() {
+  storedTitles.length = 0;
+  setCookie("storedTitles", "", 30);
+  document.querySelector("#storedTitles").innerHTML =
+    "<p>No Stored Titles Yet</p>";
+}
+
+renderStoredTitles();
+
+document.querySelector("#generateBtn").addEventListener("click", generateTitle, false);
+document.querySelector("#storeBtn").addEventListener("click", storeTitle, false);
+document.querySelector("#clearBtn").addEventListener("click", clearStoredTitles, false);
+
+
+
+// Cookie Handlers
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
